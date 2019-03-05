4a9b705be1a978e62013aff8b41db6853b7da01d
Rutvik Choudhary <rchoudhary11235@gmail.com>
2019-03-05 03:21:27 -0600

    Lol I f-ed up there and put the A,P,L bits in the control store

--- csv-to-bits.py
+++ csv-to-bits.py
@@ -11,14 +11,10 @@ with open('states.csv') as csvfile:
     next(csvreader)
     for row in csvreader:
         state_num = stoi(row[0])
-        EVCND = stoi(row[3])
+        EVCOND = stoi(row[3])
         J = stoi(row[4])
         MOD = stoi(row[5])
         DEC = stoi(row[6])
-        L = stoi(row[7])
-        P = stoi(row[8])
-        A = stoi(row[9])
-        f.write('{:051b}{:1b}{:07b}{:02b}{:1b}{:1b}{:1b}{:1b}\n'
-            .format(0, EVCND, J, MOD, DEC, L, P, A))
+        f.write('{:054b}{:1b}{:07b}{:02b}{:1b}\n'.format(0, EVCOND, J, MOD, DEC))
 
 f.close()
--- state_machine.v
+++ state_machine.v
@@ -3,7 +3,10 @@
 module StateMachine(
     input clk,
     input [15:0] family_bits,
-    input COND
+    input COND,
+    input L,
+    input P,
+    input A
 );
 
 reg   [6:0] address;
@@ -15,13 +18,10 @@ initial begin
 end
 
 wire [63:0] CS_BITS = control_store[address];
-wire        EVCND   = CS_BITS[13];
-wire  [7:0] J       = CS_BITS[12:6];
-wire  [1:0] MOD     = CS_BITS[5:4];
-wire        DEC     = CS_BITS[3];
-wire        L       = CS_BITS[2];
-wire        P       = CS_BITS[1];
-wire        A       = CS_BITS[0];
+wire        EVCOND  = CS_BITS[10];
+wire  [7:0] J       = CS_BITS[9:3];
+wire  [1:0] MOD     = CS_BITS[2:1];
+wire        DEC     = CS_BITS[0];
 
 wire J2_toggle =  MOD[1] &  MOD[0] & L;
 wire J1_toggle =  MOD[1] & ~MOD[0] & P;
@@ -36,7 +36,7 @@ Encoder16To4 _(
 wire [6:0] decode_target = {family_number, 3'b0};
 
 wire [6:0] non_fetch_address = (DEC == 1) ? decode_target : jump_target;
-wire [6:0] next_state_address = (COND == 1 && EVCND == 1) ? non_fetch_address : 7'd104;
+wire [6:0] next_state_address = (COND == 0 && EVCOND == 1) ? 7'd104 : non_fetch_address;
 
 always @(posedge clk) begin
     address <= next_state_address;
