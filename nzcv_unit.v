99d55bf9472623fcdb5b35f77cfc8c1101910b1d
Carter Airhart <airhartcarter@utexas.edu>
2019-03-19 16:02:52 -0500

    fixed the nzcv_unit wasnt compiling

--- nzcv_unit.v
+++ nzcv_unit.v
@@ -1,10 +1,12 @@
-module alu(
+module nzcv_unit(
            input [3:0] nzcv_input,// ALU Selection
 					 input s_input,
 					 input [3:0] opcode_input,
-					 output operate
+					 output operated
     );
     reg [3:0] nzcv;
+		reg operate;
+		assign operated = operate;
     always @(*)
     begin
 				nzcv = s_input ? nzcv_input : nzcv;
@@ -26,17 +28,17 @@ module alu(
         4'b0111: // 
 					operate = !nzcv[0];
         4'b1000: //
-					operate = nzcv[1] && !nzcv[2]
+					operate = nzcv[1] && !nzcv[2];
         4'b1001: //
-					operate = !nzcv[1] || nzcv[2]
+					operate = !nzcv[1] || nzcv[2];
         4'b1010: //
 					operate = (nzcv[3] && nzcv[0]) || (!nzcv[3] && !nzcv[0]);
         4'b1011: //
 					operate = (nzcv[3] && !nzcv[0]) || (!nzcv[3] && nzcv[0]);
         4'b1100: //
-					operate = (nzcv[2] && (nzcv[3] || nzcv[0]) || (!nzcv[3] && !nzcv[0]);
+					operate = (nzcv[2] && (nzcv[3] || nzcv[0])) || (!nzcv[3] && !nzcv[0]);
         4'b1101: //
-					operate = (nzcv[2] || (nzcv[3] && !nzcv[0]) || (!nzcv[3] && nzcv[0]);
+					operate = (nzcv[2] || (nzcv[3] && !nzcv[0])) || (!nzcv[3] && nzcv[0]);
         4'b1110: //
 					operate = 1;
         4'b1111: //
