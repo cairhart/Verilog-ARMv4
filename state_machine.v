a8e6dc5496661fc581730bf8381610d9c4d66cf8
Carter Airhart <airhartcarter@utexas.edu>
2019-03-21 19:20:52 -0500

    FAMILY SMASH

--- state_machine.v
+++ state_machine.v
@@ -32,6 +32,20 @@ wire J1_toggle =  MOD[1] & ~MOD[0] & P;
 wire J0_toggle = ~MOD[1] &  MOD[0] & A;
 wire [6:0] jump_target = {J[6:3], J[2] | J2_toggle, J[1] | J1_toggle, J[0] | J0_toggle};
 
+wire [3:0] family_smasher;
+assign family_smasher = (family_number == 5 ||
+							 family_number == 6 ||
+							 family_number == 7 ||
+							 family_number == 15) ? 4'b1111 :
+							 (family_number == 8 ||
+							 family_number == 9 ||
+							 family_number == 10 ||
+							 family_number == 11) ? 4'b0101 :
+							 (family_number == 12) ? 4'b1000 :
+							 (family_number == 13) ? 4'b1001 :
+							 (family_number == 14) ? 4'b0111 :
+							 family_number;
+
 wire [6:0] decode_target = {family_number, 3'b0};
 
 wire [6:0] non_fetch_address = DEC ? decode_target : jump_target;
