f62638cdce0cd14ffdfa7bdbf94ce031f2f42171
Carter Airhart <airhartcarter@utexas.edu>
2019-03-22 12:17:37 -0500

    fixed dabs in microsequencer state in state_machine

--- state_machine.v
+++ state_machine.v
@@ -51,7 +51,7 @@ wire [6:0] decode_target = {family_number, 3'b0};
 
 wire [6:0] non_fetch_address = DEC ? decode_target : jump_target;
 wire [6:0] next_state_address = (COND == 0 && EVCOND == 1) ? 7'd104 : non_fetch_address;
-wire [6:0] next_state_final = ( address == 44 && IR_20 == 1) ? 7'd104 : next_state_address;
+wire [6:0] next_state_final = ( address == 44 && IR_20 == 1) ? 7'd46: next_state_address;
 
 always @(posedge clk) begin
     if (rst == 1) address <= 7'd104; // Reset to fetch state
