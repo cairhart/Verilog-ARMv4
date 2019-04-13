2ccd610a5ee799be7e590f23c0f5a478ebad7f27
Rutvik Choudhary <rchoudhary11235@gmail.com>
2019-04-13 15:22:09 -0500

    Third time's the charm...??

--- state_machine_EXTENDED.v
+++ state_machine_EXTENDED.v
@@ -13,6 +13,7 @@ module StateMachineEXTENDED(
     input           RM_CNTR_DONE,
     input   [1:0]   OP,
     input           Z,
+    input           is_VEC,
 
     output [63:0]   CS_BITS
 );
@@ -36,11 +37,11 @@ wire       DOT_PROD_RST  = CS_BITS[16];
 wire       RM_CNTR_JMP   = CS_BITS[15];
 wire       RM_CNTR_LOOP  = CS_BITS[14];
 
-wire J3_toggle =  DOT_PROD_RST &  Z;
-wire J2_toggle =  (MOD[1] &  MOD[0] & ST) | (VEC_OP_BR & OP[1]);
-wire J1_toggle =  (MOD[1] & ~MOD[0] & PL) | (VEC_OP_BR & OP[0]);
-wire J0_toggle = ~MOD[1] &  MOD[0] & A;
-wire [6:0] jump_target = {J[6:4], J[3] | J3_toggle, J[2] | J2_toggle, J[1] | J1_toggle, J[0] | J0_toggle};
+wire J3_toggled = J[3] | (DOT_PROD_RST &  Z);
+wire J2_toggled = J[2] | ( MOD[1] &  MOD[0] & ST);
+wire J1_toggled = J[1] | ( MOD[1] & ~MOD[0] & PL);
+wire J0_toggled = J[0] | (~MOD[1] &  MOD[0] & A);
+wire [6:0] jump_target = {J[6:4], J3_toggled, J2_toggled, J1_toggled, J0_toggled};
 
 wire [3:0] family_code = (family_number == 5 ||
                              family_number ==  6 ||
@@ -61,8 +62,11 @@ wire [3:0] family_code = (family_number == 5 ||
                                        : family_number;
 
 wire [6:0] decode_target = {family_code, 3'b0};
+wire dt_2_toggled = decode_target[2] | (is_VEC & OP[1]);
+wire dt_1_toggled = decode_target[1] | (is_VEC & OP[0]);
+wire [6:0] decode_target_modfied = {decode_target[6:3], dt_2_toggled, dt_1_toggled, decode_target[0]};
 
-wire [6:0] non_fetch_address = (DEC == 1) ? decode_target : jump_target;
+wire [6:0] non_fetch_address = (DEC == 1) ? decode_target_modfied : jump_target;
 wire [6:0] next_state_address = (COND == 0 && EVCOND == 1) ? 7'd104 : non_fetch_address;
 wire [6:0] next_state_final = (RM_CNTR_JMP == 1 && RM_CNTR_DONE == 1) ? 7'd104: next_state_address;
 
