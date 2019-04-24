425b7943d7657bf5bd6bbb892525a86fb59ba33e
Rutvik Choudhary <rchoudhary11235@gmail.com>
2019-04-24 14:42:05 -0500

    Wrote assembly file for testing extension

---
+++ asm_tests/ext_test.asm
@@ -0,0 +1,20 @@
+; init first vector
+MOV R0, #11
+MOV R1, #4
+MOV R2, #7
+MOV R3, #15
+MOV R4, #9
+
+; init second vector
+MOV R5, #1
+MOV R6, #0
+MOV R7, #2
+MOV R8, #2
+MOV R9, #6
+
+; init scalar
+MOV R11, #3
+
+; extension operation goes here
+
+MRS R0, CPSR
