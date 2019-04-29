95a6d83bba663ab17ca1e4ff93be8e80425fb508
ThomasPostMeridian <tompmcr@sbcglobal.net>
2019-04-28 22:12:03 -0500

    Uncommitted changes, must have been an old synthesis run

--- ARMv4_synthesis/ARMv4_synthesis.runs/impl_2/ARMv4.tcl
+++ ARMv4_synthesis/ARMv4_synthesis.runs/impl_2/ARMv4.tcl
@@ -65,10 +65,6 @@ start_step init_design
 set ACTIVE_STEP init_design
 set rc [catch {
   create_msg_db init_design.pb
-  set_param power.BramSDPPropagationFix 1
-  set_param power.enableUnconnectedCarry8PinPower 1
-  set_param power.enableCarry8RouteBelPower 1
-  set_param power.enableLutRouteBelPower 1
   create_project -in_memory -part xczu17eg-ffve1924-3-e
   set_property design_mode GateLvl [current_fileset]
   set_param project.singleFileAddWarning.threshold 0
